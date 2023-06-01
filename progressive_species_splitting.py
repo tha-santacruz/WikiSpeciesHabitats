73a3173690561b1c4e9a32422eaa4a85afc53e25
Nicola Antonio Santacroce <nicola.santacroce@epfl.ch>
2023-06-01 13:32:19 +0200

    trimmed fields of progressive split files

--- progressive_species_splitting.py
+++ progressive_species_splitting.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     num_steps = 10
     for random_state in [1,2,3,4,5]:
         ## Read half2 data
-        half_2_data = pd.read_json(f"./final_data/{random_state}_L1_species_based_test_data.json", orient="records")
+        half_2_data = pd.read_json(f"./final_data/{random_state}_L1_species_based_test_data.json", orient="records")[["set_based_class","species_based_class","species_key"]]
         ## Find a splitting that ensures a high overlapping of species between both equally long splits
         max_overlap = 0
         best_rs = 0
@@ -35,7 +35,8 @@ if __name__ == "__main__":
         t2_spe = [item for sublist in t2_spe for item in sublist]
         t2_spe = list(set(t2_spe))
         ## This one is gonna be used as a pool for train examples
-        t3 = pd.read_json(f"./final_data/{random_state}_L1_species_based_train_data.json", orient="records").sample(len(t1), random_state=42)
+        t3 = pd.concat([pd.read_json(f"./final_data/{random_state}_L1_species_based_train_data.json", orient="records"),pd.read_json(f"./final_data/{random_state}_L1_species_based_val_data.json", orient="records")])[["set_based_class","species_based_class","species_key"]]
+        #t3 = pd.read_json(f"./final_data/{random_state}_L1_species_based_train_data.json", orient="records").sample(len(t1), random_state=42) ## old method
         ## To store chosen examples from t3
         t3_subset = t3
         ## To store unused examples from t2
@@ -45,12 +46,12 @@ if __name__ == "__main__":
         for i in range(0,num_steps+1):
             frac = i/num_steps
             if frac > 0:
-                ## Remove examples from t3 subset
-                t3_subset = t3_subset.drop(t3_subset.sample(frac = 1/(num_steps+1-i), random_state = random_state).index)
                 ## Add examples to t2 subset and remove them from pooling
                 t2_sampled = t2_pool.sample(frac = 1/(num_steps+1-i), random_state = random_state)
                 t2_pool = t2_pool.drop(t2_sampled.index)
                 t2_subset = pd.concat([t2_subset, t2_sampled])
+                ## Remove examples from t3 subset
+                t3_subset = t3_subset.drop(t3_subset.sample(len(t2_sampled), random_state = random_state).index)
             ## Create train/val split
             t4 = pd.concat([t3_subset, t2_subset])
             ## Compute number of species in intersection, union, etc...
@@ -64,13 +65,13 @@ if __name__ == "__main__":
             ## Log metrics
             splits_stats = pd.concat([splits_stats, pd.DataFrame(data=[random_state,frac,len(intersection),len(union),len(t4_spe),len(t1_spe)]).T])
             ## Put 5% of examples in val
-            t5 = t4.sample(frac=0.05, random_state=random_state)
+            t5 = t4.sample(frac=0.1, random_state=random_state)
             t4 = t4.drop(t5.index)
             ## Save train and val
             t4.reset_index(drop=True).to_json(f"./final_data/{random_state}_L1_progressive_{int(frac*100)}%_train_data.json", orient="records")
             t5.reset_index(drop=True).to_json(f"./final_data/{random_state}_L1_progressive_{int(frac*100)}%_val_data.json", orient="records")
         ## Save test
-        t1.reset_index(drop=True).to_json(f"./final_data/{random_state}_L1_progressive_test_data.json")
+        t1.reset_index(drop=True).to_json(f"./final_data/{random_state}_L1_progressive_test_data.json", orient="records")
     ## Save metrics
     splits_stats.columns=["random_state","frac","species_inter","species_union","train_species","test_species"]
     splits_stats.to_json("progressive_splits_stats.json", orient="records")
